c0743771364c3c44f046cb1f1447c53c45420253
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 16:42:16 -0600

    refactored tip_calc2.py to use dict for tip conversion

--- PyPart1/tip_calc2.py
+++ PyPart1/tip_calc2.py
@@ -1,3 +1,19 @@
 subtotal = float(input('Total bill amount? '))
 service = input('Level of service? ').lower()
 split = int(input('Split how many ways? '))
+tips = {'good' : 1.2, 'fair' : 1.15, 'bad':1.1}
+tip = tips[service]
+total = tip * subtotal
+print('Tip amount: ${:.2f}'.format(tip))
+print('Total amount: ${:.2f}'.format(total))
+print('Amount per person: {:.2f}'.format(total / split))
+# if service == 'good':
+
+# elif service == 'fair':
+#     print('Tip amount: ${:.2f}'.format(.15 * subtotal))
+#     print('Total amount: ${:.2f}'.format(1.15 * subtotal))
+#     print('Amount per person: {}'.format(total= total / split))
+# elif service == 'bad':
+#     print('Tip amount: ${:.2f}'.format(.1 * subtotal))
+#     print('Total amount: ${:.2f}'.format(1.1 * subtotal))
+#     print('Amount per person: {}'.format(total= total / split))
