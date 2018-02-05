cd9f33d562e774beffd9892a1448419fef5d0905
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 16:18:44 -0600

    added two possibilities for output value, float or integer in comment for degree_conversion.py

--- PyPart1/degree_conversion.py
+++ PyPart1/degree_conversion.py
@@ -1,3 +1,4 @@
 celsius = int(input('Temperature in C? '))
-print(str(celsius * 1.8 + 32) + ' F')
+print(str(int(celsius * 1.8 + 32)) + ' F')
 #conversion to float or integer value?
+#print(str(int(celsius * 1.8 + 32)) + ' F')
