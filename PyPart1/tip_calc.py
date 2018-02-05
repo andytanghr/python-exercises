6a42d2fa9f5a2012dde0ef37f13d2e1beadc3be9
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 16:43:53 -0600

    refactored tip_calc.py and tip_calc2.py to use dict for tip conversion

--- PyPart1/tip_calc.py
+++ PyPart1/tip_calc.py
@@ -1,12 +1,17 @@
 subtotal = float(input('Total bill amount? '))
 service = input('Level of service? ').lower()
-#use a dictionary to convert level of service to percent tip?
-if service == 'good':
-    print('Tip amount: ${:.2f}'.format(.2 * subtotal))
-    print('Total amount: ${:.2f}'.format(1.2 * subtotal))
-elif service == 'fair':
-    print('Tip amount: ${:.2f}'.format(.15 * subtotal))
-    print('Total amount: ${:.2f}'.format(1.15 * subtotal))
-elif service == 'bad':
-    print('Tip amount: ${:.2f}'.format(.1 * subtotal))
-    print('Total amount: ${:.2f}'.format(1.1 * subtotal))
+tips = {'good' : 1.2, 'fair' : 1.15, 'bad':1.1}
+tip = tips[service]
+total = tip * subtotal
+print('Tip amount: ${:.2f}'.format(tip))
+print('Total amount: ${:.2f}'.format(total))
+# #use a dictionary to convert level of service to percent tip?
+# if service == 'good':
+#     print('Tip amount: ${:.2f}'.format(.2 * subtotal))
+#     print('Total amount: ${:.2f}'.format(1.2 * subtotal))
+# elif service == 'fair':
+#     print('Tip amount: ${:.2f}'.format(.15 * subtotal))
+#     print('Total amount: ${:.2f}'.format(1.15 * subtotal))
+# elif service == 'bad':
+#     print('Tip amount: ${:.2f}'.format(.1 * subtotal))
+#     print('Total amount: ${:.2f}'.format(1.1 * subtotal))
