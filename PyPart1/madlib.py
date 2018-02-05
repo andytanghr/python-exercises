83758d478269a0bd78e95ec68e60f27582bb4c8c
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 16:08:36 -0600

    finished madlib.py, started day_of_week.py

---
+++ PyPart1/madlib.py
@@ -0,0 +1,6 @@
+print('Please fill in the blanks below:')
+print('Hello, my name is ____(name)____, and today, I\'m going to ____(verb)____.')
+name = input('What is your name? ')
+verb = input('What action do you take? ' )
+print('Hello, my name is {}, and today, I\'m going to {}.'.format(name, verb))
+
--- logic.py
+++ logic.py
@@ -2,7 +2,7 @@ age = int(input('How old are you? '))
 
 if age >= 21:
     print('You get free beer')
-elif age <= 18:
+elif age > 18:
     print('Go home kid')
 else:
     print('No beer for you')
