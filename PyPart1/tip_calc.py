f5872e0548c00946ab0c621cced016bd40a16877
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 16:35:47 -0600

    finished tip_calc.py, possible refactor in future

--- PyPart1/tip_calc.py
+++ PyPart1/tip_calc.py
@@ -1,11 +1,12 @@
 subtotal = float(input('Total bill amount? '))
 service = input('Level of service? ').lower()
+#use a dictionary to convert level of service to percent tip?
 if service == 'good':
-    print('Tip amount: ${}'.format(.2 * subtotal))
-    print('Total amount: $'.format(1.2 * subtotal))
+    print('Tip amount: ${:.2f}'.format(.2 * subtotal))
+    print('Total amount: ${:.2f}'.format(1.2 * subtotal))
 elif service == 'fair':
-    print('Tip amount: ${}'.format(.15 * subtotal))
-    print('Total amount: $'.format(1.15 * subtotal))
-elif service = 'bad'
-    print('Tip amount: ${}'.format(.1 * subtotal))
-    print('Total amount: $'.format(1.1 * subtotal))
+    print('Tip amount: ${:.2f}'.format(.15 * subtotal))
+    print('Total amount: ${:.2f}'.format(1.15 * subtotal))
+elif service == 'bad':
+    print('Tip amount: ${:.2f}'.format(.1 * subtotal))
+    print('Total amount: ${:.2f}'.format(1.1 * subtotal))
---
+++ PyPart1/tip_calc2.py
@@ -0,0 +1,3 @@
+subtotal = float(input('Total bill amount? '))
+service = input('Level of service? ').lower()
+split = int(input('Split how many ways? '))
