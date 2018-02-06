9adc081d1d4a82e2ec4df48c246d748eb90471fd
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-06 10:51:27 -0600

    edited multplyVectors.py

--- PyPart2/multiplyVectors.py
+++ PyPart2/multiplyVectors.py
@@ -1,6 +1,6 @@
 vector1 = [2, 4, 5]
 vector2 = [2, 3, 6]
-resultant = []
+product = []
 for number in range(0, len(vector1)):
-    resultant.append(vector1[number] * vector2[number])
-print(resultant)
+    product.append(vector1[number] * vector2[number])
+print(product)
