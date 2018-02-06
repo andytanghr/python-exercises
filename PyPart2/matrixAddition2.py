0a11b0997777ebfa41f8082687b972b1b5b38a24
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-06 11:11:24 -0600

    added matrixAddition2.py

--- PyPart2/matrixAddition2.py
+++ PyPart2/matrixAddition2.py
@@ -6,6 +6,7 @@ matrix2 = [[3, 1], [-1, 8], [-3, 3]]
 # need to use nested loops to traverse each list's element to add to corresponding of another list's element
 # summation elements for every line of resultant is generated, then appended to resultant
 # "line" is synonym to "row"
+# solution should work for any pair of matrices of same size
 for row in range(0, len(matrix1)):
     line = []
     for column in range(0, len(matrix1[row])):
