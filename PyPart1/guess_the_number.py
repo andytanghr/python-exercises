229101bf8a4acb6aa26c82161c9ddfff4524d906
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 18:36:57 -0600

    added high/low feature to guess_the_number.py

---
+++ PyPart1/guess_the_number.py
@@ -0,0 +1,10 @@
+print('I\'m thinking of a number between 1 and 10.')
+guess = int(input('What\'s the number?' ))
+secret = 5
+while guess != secret:
+    if guess < secret:
+        print('{} is too low.'.format(guess))
+    elif guess > secret:
+        print('{} is too high.'.format(guess))
+    guess = int(input('What\'s the number?' ))    
+print('Yes! You win!')
