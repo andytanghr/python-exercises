78abbce03c952592efbcc6a29c2c11029c71b7f4
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 18:52:22 -0600

    added replay option, probs refactor into functions for game play in guess_the_number.py

--- PyPart1/guess_the_number.py
+++ PyPart1/guess_the_number.py
@@ -1,13 +1,32 @@
 import random
 
 print('I\'m thinking of a number between 1 and 10.')
+lives = 5
+print('You have {} guesses left'.format(lives))
 guess = int(input('What\'s the number? '))
 secret = random.randint(1, 10)
-print(secret)
+# print(secret)
 while guess != secret:
     if guess < secret:
         print('{} is too low.'.format(guess))
+        print('You have {} guesses left'.format(lives))
     elif guess > secret:
         print('{} is too high.'.format(guess))
-    guess = int(input('What\'s the number? '))    
-print('Yes! You win!')
+        print('You have {} guesses left'.format(lives))
+    guess = int(input('What\'s the number? '))
+    lives = lives - 1
+    if lives == 0:
+        print('You ran out of guesses!')
+        break
+    if guess == secret:
+        print('Yes! You win!')
+        again = input('Do you want to play again (Y or N)? ')
+        if again == 'Y':
+            lives = 5
+            print('You have {} guesses left'.format(lives))
+            guess = int(input('What\'s the number? '))
+            secret = random.randint(1, 10)
+        else:
+            print('Bye!')
+            break
+    
