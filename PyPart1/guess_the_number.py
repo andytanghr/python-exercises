9c68b0ae1559fa00b1f9521b0031ff36af9d2529
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-05 18:41:28 -0600

    added randint(1, 10) for to-be-guessed number to guess_the_number.py

--- PyPart1/guess_the_number.py
+++ PyPart1/guess_the_number.py
@@ -1,10 +1,13 @@
+import random
+
 print('I\'m thinking of a number between 1 and 10.')
-guess = int(input('What\'s the number?' ))
-secret = 5
+guess = int(input('What\'s the number? '))
+secret = random.randint(1, 10)
+print(secret)
 while guess != secret:
     if guess < secret:
         print('{} is too low.'.format(guess))
     elif guess > secret:
         print('{} is too high.'.format(guess))
-    guess = int(input('What\'s the number?' ))    
+    guess = int(input('What\'s the number? '))    
 print('Yes! You win!')
