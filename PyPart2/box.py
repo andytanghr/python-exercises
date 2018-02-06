e216b8fe1f40ddac5024369364c3d7313bc7f776
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-06 14:23:59 -0600

    added box.py

--- PyPart2/box.py
+++ PyPart2/box.py
@@ -1,2 +1,10 @@
 width = int(input('Width? '))
 height = int(input('Height? '))
+
+topBottom = '*' * width
+
+print(topBottom)
+for row in range(0, height-2):
+    spaces = ' ' * (width-2)
+    print('*' + spaces + '*')
+print(topBottom)
