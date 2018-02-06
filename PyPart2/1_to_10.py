583cc935878c5a075752740278945b363e451abf
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-06 15:01:19 -0600

    added multiplicationTable.py

--- PyPart2/1_to_10.py
+++ PyPart2/1_to_10.py
@@ -1,2 +1,8 @@
-for index in range(1, 11):
-    print(index)
+height = int(input('Triangle height? '))
+width = 1
+
+for row in range(height):
+    print(' ' * height + '*' * width + ' ' * height)
+    width += 2
+    height -= 1
+    
---
+++ PyPart2/multiplicationTable.py
@@ -0,0 +1,8 @@
+for multiplicand in range(1, 11):
+  for multiplier in range(1, 11):
+    product = multiplicand * multiplier
+    print(str(multiplicand) + ' X ' + str(multiplier) + ' = ' + str(product))
+
+# 1 to 10
+# 2 to 10
+# 3 to 10
