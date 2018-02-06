5c63c254237c5658137ec1b5ddce3144a2e87edc
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-06 10:38:49 -0600

    added positiveNumbers2.py

---
+++ PyPart2/positiveNumbers2.py
@@ -0,0 +1,6 @@
+listOfNums = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+listOfPosNums = []
+for number in listOfNums:
+    if number > 0:
+        listOfPosNums.append(number)
+print(listOfPosNums)
