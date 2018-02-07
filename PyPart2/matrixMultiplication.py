cc33f8400c99fc52d8852fbb084f86df7e811438
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-07 13:34:43 -0600

    finished matrixMultiplication.py

--- PyPart2/matrixMultiplication.py
+++ PyPart2/matrixMultiplication.py
@@ -1,25 +1,12 @@
-# matrixA = [[2, -2], [5, 3]]
-# matrixB = [[-1, 4], [7, -6]]
-# product = []
-# line = []
-# for rowA in range(0, len(matrixA)):
-#     line[rowA] = []
-#     for columnB in range(0, len(matrixA)):
-#         element = 0
-#         for rowB in range(0, len(matrixA)):
-#             element = (matrixA[rowA][rowB] * matrixB[rowB][columnB])
-#             line.append(element)
-#     product.append(line)
-# print(product)
-
-matrixA = [[2, -2], [5, 3]]
+matrixA = [[2, -2], [5, 3], [1, 3]]
 matrixB = [[-1, 4], [7, -6]]
 product = []
 for row in range(len(matrixA)):
-    product.append(row)
+    line = []
     for column in range(len(matrixB)):
         element = 0
         for width in range(len(matrixB)):
             element += matrixA[row][width] * matrixB[width][column]
-        product[row][column].append(element)
+        line.append(element)
+    product.append(line)
 print(product)
