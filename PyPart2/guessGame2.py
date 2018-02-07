7561b7d3f2df182caec9cf51319ac63a9ca09e01
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-07 11:34:09 -0600

    finished three versions of guessGame2.py

--- PyPart2/guessGame2.py
+++ PyPart2/guessGame2.py
@@ -1,34 +1,53 @@
 import random
 
 target = int(input('Enter a number between 0 and 100 for the computer to guess: '))
-guessed = False
-guesses = []
+attempts = 0
+# guessed = False
+# guesses = []
+
 # v1: computer guesses incrementally from 0 to 100:
-for guess in range(0, 101):
-  print('Guess #{}: {}'.format(guess, guess))
-  if guess == target:
-    print('The computer guessed correctly after {} tries.'.format(guess))
-    break
-  elif guess > target:
-    print('The computer guessed too high.')
-  else:
-    print('The computer guessed too low.')
-# v2: computer guesses randomly
-while guessed != True:
-  guess = random.randint(0, 100)
-  print('Guess #{}: {}'.format(guess, guess))
-  if guess == target:
-    print('The computer guessed correctly after {} tries.'.format(guess))
-    guessed = True
-    break
-  elif guess > target:
-    print('The computer guessed too high.')
-  else:
-    print('The computer guessed too low.')
-
-
-# v3: computer guesses 50 first, then goes
-# v4: computer divides and conquers by binary guess
-# while guessed != True:
-#   guess = random.randint(0, 100)
+# for guess in range(0, 101):
+#   print('Guess #{}: {}'.format(guess, guess))
 #   if guess == target:
+#     print('The computer guessed correctly after {} tries.'.format(guess))
+#     break
+#   elif guess > target:
+#     print('The computer guessed too high.')
+#   else:
+#     print('The computer guessed too low.')
+
+# v2: computer guesses randomly
+# attempts = 1
+# while True:
+#   guess = random.randint(0, 100)
+#   print('Guess #{0}: {1}'.format(attempts, guess))
+#   if guess == target:
+#     print('The computer guessed correctly after {} tries.'.format(attempts))
+#     break
+#   elif guess > target:
+#     print('The computer guessed too high.')
+#     attempts += 1
+#   else:
+#     print('The computer guessed too low.')
+#     attempts += 1
+
+
+# v3: computer divides and conquers by binary guess
+guess = random.randint(0, 100)
+high = 100
+low = 0
+while True:
+  print('Guess #{}: {}'.format(attempts,guess))
+  if guess == target:
+    print('The computer guessed correctly after {} tries.'.format(attempts))
+    break
+  elif guess > target:
+    print('The computer guessed too high.')
+    attempts += 1
+    high = guess
+    guess = random.randint(low, high)
+  else:
+    print('The computer guessed too low.')
+    attempts += 1
+    low = guess
+    guess = random.randint(low, high)
