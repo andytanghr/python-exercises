c3756e5030f897eb78be08ca1b33783122b6023a
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-08 15:38:43 -0600

    added drawCircle.py

---
+++ turtle/drawCircle.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+def drawCircle():
+  circle(150)
+
+if __name__ == '__main__':
+  drawCircle()
+  mainloop()
+  
---
+++ turtle/drawStar.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+def drawStar():
+  for line in range(5):
+    right(144)
+    forward(180)
+
+if __name__ == '__main__':
+  drawStar()
+  mainloop()
