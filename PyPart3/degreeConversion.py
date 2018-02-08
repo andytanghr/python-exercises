08f330af5aff69daf13f2c55bd59b29f0f9df5d6
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-08 14:45:52 -0600

    added graphing to degreeConversion.py

--- PyPart3/degreeConversion.py
+++ PyPart3/degreeConversion.py
@@ -1,2 +1,13 @@
+import matplotlib.pyplot as plot
+
 def convertCtoF(temperature):
   return temperature * 1.8 + 32
+
+xs = list(range(-50, 50))
+ys = []
+
+for x in xs:
+  ys.append(convertCtoF(x))
+
+plot.plot(xs, ys)
+plot.show()
