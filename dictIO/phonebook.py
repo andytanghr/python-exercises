158158d87842866bcd5ac1db46310dc030b50227
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-12 15:01:42 -0600

    added phonebook.py

---
+++ dictIO/phonebook.py
@@ -0,0 +1,47 @@
+'''
+data structure is a dict:
+'name': 'phone number'
+
+def menu()
+  # displays menu
+  # input number executes/returns that function
+  menu = '''
+  Electronic Phone Book
+  =====================
+  1. Look up an entry
+  2. Set an entry
+  3. Delete an entry
+  4. List all entries
+  5. Save entries
+  6. Restore saved entries
+  7. Quit
+  '''
+  
+
+
+
+def printContact(name):
+  #takes in name, accesses dict to print out number
+
+def addContact(name, number):
+  # adds key and value to data
+
+def delContact(name)
+  # del
+  # prompt again to be sure
+
+def listAll():
+  # iterate through data printing out name, number
+
+def quit():
+
+def saveContacts():
+
+
+
+def restoreContacts():
+
+
+
+
+'''
