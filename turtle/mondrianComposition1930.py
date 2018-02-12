62da6de1896d049117ed818682b91b9726495a31
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-12 01:24:32 -0600

    added mondrianComposition1930.py

---
+++ turtle/mondrianComposition1930.py
@@ -0,0 +1,19 @@
+from shapes import *
+
+setup(width=568, height=568)
+bgcolor('white')
+
+
+# draw blue
+setpos(-142,-142)
+drawSquare(129, True, (1, 93, 158))
+
+
+# draw yellow
+setpos(90,-142)
+
+# draw red
+
+# draw thicker line
+
+# draw thinner line
