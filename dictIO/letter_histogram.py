b72ae116fc4c079a863b98f2d94cc5a4b3e17da2
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-02-12 11:27:31 -0600

    finished letter_histogram.py, added word_histogram.py

---
+++ dictIO/letter_histogram.py
@@ -0,0 +1,19 @@
+
+def letter_histogram():
+  word = input('Input word enclosed by quote marks to tally its characters: ')
+  print(type(word))
+  histogram = {}
+  for char in word:
+    if char not in histogram:
+      histogram[char] = 1
+    else:
+      histogram[char] += 1
+  print(histogram)
+  return histogram
+
+if __name__ == '__main__':
+  letter_histogram()
+
+
+
+
